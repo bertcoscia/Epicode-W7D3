06d4bd61211df6389d800cd6ff5fb184dcab40a6
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-06-12 15:27:49 +0200

    adds delete button

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,15 +17,18 @@ const fetchBooks = () => {
       // creo un riferimento al .row
       const row = document.querySelector(".row");
 
-      const deleteCard = () => {
+      /* const deleteCard = () => {
         console.log("hello world");
-      };
+        const actualCard = document.getElementById(`${book.asin}`);
+        actualCard.remove();
+      }; */
 
       // creo una card per ogni elemento
       booksObject.forEach((book) => {
         const card = document.createElement("div");
         card.classList.add("card");
         card.setAttribute("id", `${book.asin}`);
+
         // genero l'immagine della card
         const cardImg = document.createElement("img");
         cardImg.setAttribute("src", `${book.img}`);
@@ -33,22 +36,26 @@ const fetchBooks = () => {
         cardImg.classList.add("object-fit-cover");
         cardImg.setAttribute("alt", `Cover of the book ${book.title}`);
         card.appendChild(cardImg);
+
         // genero il div card-body
         const cardBody = document.createElement("div");
         cardBody.classList.add("card-body");
         cardBody.classList.add("postion-relative");
         card.appendChild(cardBody);
+
         // genero il card-title
         const cardTitle = document.createElement("h5");
-        cardTitle.classList.add("card-title");
         cardTitle.innerText = `${book.title}`;
+        cardTitle.classList.add("card-title");
         cardBody.appendChild(cardTitle);
+
         // genero il card-text
         const cardText = document.createElement("p");
         cardText.classList.add("card-text");
         cardText.innerText = `Category: ${book.category}
         Price: $${book.price}`;
         cardBody.appendChild(cardText);
+
         // genero il bottone scarta
         const discardBtn = document.createElement("button");
         discardBtn.classList.add("btn");
@@ -57,9 +64,12 @@ const fetchBooks = () => {
         discardBtn.classList.add("position-absolute");
         discardBtn.classList.add("bottom-0");
         discardBtn.classList.add("start-0");
-        discardBtn.addEventListener("click", deleteCard);
         discardBtn.innerText = "Delete book";
+        discardBtn.addEventListener("click", () => {
+          card.classList.add("d-none");
+        });
         cardBody.appendChild(discardBtn);
+
         // genero il div che contiene la card
         const col = document.createElement("div");
         col.classList.add("col");
